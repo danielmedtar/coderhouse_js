418bc40ed698f1ea1396aff99e18130a8fdccbf8
danielmedtar <dmedinat.ar@gmail.com>
2021-03-20 14:32:27 -0300

    Agregando archivos

---
+++ 1_y_2/clase_1/index.js
@@ -0,0 +1,17 @@
+//Prompt
+let nombre = prompt("ingrese su nombre");
+let edad = prompt("Ingrese su edad")
+edad = parseInt(edad); //Como edad en el prompt lo toma como un string, con esto lo convertimos en numero
+
+
+
+console.log(nombre)
+
+alert("Bienvenido " + nombre)
+/*let gatito = "Ruben"
+
+const mensaje = 'Desde el index html'
+
+gatito = "Daniel"
+
+console.log(mensaje);*/
---
+++ 1_y_2/clase_2/auth.js
@@ -0,0 +1,34 @@
+//Solo con variables separadas
+const userName = "Daniel";
+const password = "123456"
+
+let inputUserName = prompt("Ingrese su nombre de usuario");
+let inputPassword = prompt("Ingrese su clave")
+
+// console.log(inputUserName);
+// console.log(inputPassword);
+
+// if (inputUserName === userName && inputPassword === password) {
+//     console.log("Credenciales válidas")
+// } else {
+//     console.error("Credenciales inválidas")
+// }
+
+// //Tipo objeto
+// const user = {
+//     userName : "Daniel",
+//     password : "123456"
+// }
+
+
+// let inputUserName = prompt("Ingrese su nombre de usuario");
+// let inputPassword = prompt("Ingrese su clave")
+
+// console.log(inputUserName);
+// console.log(inputPassword);
+
+// if (inputUserName === user.userName && inputPassword === user.password) {
+//     console.log("Credenciales válidas")
+// } else {
+//     console.error("Credenciales inválidas")
+// }
